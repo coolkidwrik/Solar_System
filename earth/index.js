b3d9a5d60892f3144fb53bfa55f89fa036191c8e
Wrik <coolkidwrik@gmail.com>
2024-02-12 16:21:11 -0800

    abstracted code to get orbital positions

--- earth/index.js
+++ earth/index.js
@@ -2,17 +2,7 @@ import * as THREE from 'three';
 import { setup } from '../utilities/setup.js';
 import { earthBuilder } from '../utilities/planet_builder/earthBuilder.js';
 import { moonBuilder } from '../utilities/planet_builder/moonBuilder.js';
-// import { getOrbitalAcceleration } from '../utilities/getOrbitalAcceleration.js';
-
-// Constants
-const G = 6.67430 * Math.pow(10, -11); // gravitational constant
-const AU = 149.6 * Math.pow(10, 6); // astronomical unit
-const SCALE = 1/AU; // scale of the model
-const TIMESTEP = 1/(60*60*24*125); // time step (1 earthday = 125 second in the model)
-// const M = mass1; // mass of earth (scaled down)
-// const m = mass2; // mass of moon (scaled down)
-// const r = orbitalRadius; // distance between earth and moon (not to scale)
-
+import { getOrbitalPosition } from '../utilities/getOrbitalPosition.js';
 
 
 // Setup and return the scene and related objects.
@@ -44,35 +34,10 @@ scene.add(moon);
 
 function updateMoonPosition() {
   // Constants
-
   const earthMass = 5.972 * Math.pow(10, 24); // kg
   const moonMass = 7.342 * Math.pow(10, 22); // kg
-  const moonOrbitRadius = 384400 * 1000*(0.125*SCALE); // meters
-
-  // Calculate gravitational force between earth and moon
-  // is given by the formula Fg = (G * M * m) / (r * r)
-  const distance = moon.position.distanceTo(earth.position); // Distance in meters, scaled
-  const force = SCALE*(G * earthMass * moonMass) / Math.pow(distance, 2);
-
-  // Calculate acceleration of the moon
-  // to get the acceleration of the moon, we divide Fg by the mass of the moon
-  const acceleration = force / moonMass;
-
-  // calculate tangential velocity of moon
-  // the centripetal force acting on the moon is given by the 
-  // formula Fc = (m * v * v) / r
-  // this also equals the force of gravity acting on the moon
-  // if we solve for v, we get v = sqrt((G * M) / r)
-  const velocity = -1*Math.sqrt(2 * acceleration * moonOrbitRadius);
-
-  // Calculate angle increment for orbit
-  const angleIncrement = (velocity * TIMESTEP) / moonOrbitRadius;
-
-  // Update moon's position
-  // the angle between the current position vector and the x-axis
-  const angle = Math.atan2(moon.position.z, moon.position.x);
-  const newX = Math.cos(angle + angleIncrement) * moonOrbitRadius;
-  const newZ = Math.sin(angle + angleIncrement) * moonOrbitRadius;
+  const moonOrbitRadius = 384400 * 1000; // meters
+  const { newX, newZ } = getOrbitalPosition(moonOrbitRadius, earthMass, moonMass, moon, earth);
   moon.position.set(newX, 0, newZ);
 }
 
@@ -90,7 +55,6 @@ sunLight.shadow.camera.far = 500;
 scene.add(sunLight);
 
 // Enable shadows on both earth and moon
-// TODO: work in progress
 earth.children[0].castShadow = true; // Assuming earth's mesh is the first child
 earth.children[0].receiveShadow = true; // Enable receiving shadows on earth
 // Enable shadow casting for moon
---
+++ utilities/getOrbitalPosition.js
@@ -0,0 +1,43 @@
+
+// Constants
+const G = 6.67430 * Math.pow(10, -11); // gravitational constant
+const AU = 149.6 * Math.pow(10, 6); // astronomical unit
+const SCALE = 1/AU; // scale of the model
+const TIMESTEP = 1/(60*60*24*125); // time step (1 earthday = 125 second in the model)
+
+// This function calculates the acceleration and velocity of the planets
+function getOrbitalPosition(orbitalRadius, mass1, mass2, moon, planet) {
+    // Constants
+  const bigMass = mass1; // kg
+  const smallMass = mass2; // kg
+  const moonOrbitRadius = orbitalRadius*(0.125*SCALE); // meters
+
+  // Calculate gravitational force between two masses: M and m
+  // is given by the formula Fg = (G * M * m) / (r * r)
+  const distance = moon.position.distanceTo(planet.position); // Distance in meters, scaled
+  const force = SCALE*(G * bigMass * smallMass) / Math.pow(distance, 2);
+
+  // Calculate acceleration of the moon
+  // to get the acceleration of the moon, we divide Fg by the mass of the moon
+  const acceleration = force / smallMass;
+
+  // calculate tangential velocity of moon
+  // the centripetal force acting on the moon is given by the 
+  // formula Fc = (m * v * v) / r
+  // this also equals the force of gravity acting on the moon
+  // if we solve for v, we get v = sqrt((G * M) / r)
+  const velocity = -1*Math.sqrt(2 * acceleration * moonOrbitRadius);
+
+  // Calculate angle increment for orbit
+  const angleIncrement = (velocity * TIMESTEP) / moonOrbitRadius;
+
+  // Update moon's position
+  // the angle between the current position vector and the x-axis
+  const angle = Math.atan2(moon.position.z, moon.position.x);
+  const newX = Math.cos(angle + angleIncrement) * moonOrbitRadius;
+  const newZ = Math.sin(angle + angleIncrement) * moonOrbitRadius;
+
+    return { newX, newZ };
+}
+
+export { getOrbitalPosition };
