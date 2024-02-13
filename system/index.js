761f98b3c552c7ffa51ed4255348f3d2bb854996
Wrik <coolkidwrik@gmail.com>
2024-02-13 02:13:53 -0800

    added changes to working sunlight

--- system/index.js
+++ system/index.js
@@ -1,6 +1,12 @@
 import * as THREE from 'three';
 import { setup } from '../utilities/setup.js';
+import { getOrbitalPosition } from '../utilities/getOrbitalPosition.js';
+
+// Import planet builders
 import { sunBuilder } from '../utilities/planet_builder/sunBuilder.js';
+import { earthBuilder } from '../utilities/planet_builder/earthBuilder.js';
+import { moonBuilder } from '../utilities/planet_builder/moonBuilder.js';
+import { saturnBuilder } from '../utilities/planet_builder/saturnBuilder.js';
 
 // Setup and return the scene and related objects.
 const {
@@ -10,22 +16,38 @@ const {
   stars
 } = setup();
 
-// Create a earth Group model and add elements of the earth to it
+
+// // // create solar system
+// let system = new THREE.Group();
+
+// create elements of the solar system
 let sun = sunBuilder();
+let earth = earthBuilder();
+let moon = moonBuilder();
+let saturn = saturnBuilder();
+
+// // add elements to the solar system
+
+
+// set initial positions of the elements
+earth.position.set(-5, 0, 0);
+moon.position.set(-5.5, 0, 0);
+
+// add elements to the scene
 scene.add(sun);
+scene.add(earth);
+scene.add(moon);
+
+
+
+
+
 
 // sunGroup.add(sunMesh);         // sunGroup.children[0] 
 // sunGroup.add(glowMesh);        // sunGroup.children[1]
 
 // ambience 
-const sunLight = new THREE.DirectionalLight(0xffffff);
-sunLight.position.set(0, 0, 0); // Sun is at the center
-sunLight.castShadow = true; // Enable shadow casting
-sunLight.shadow.mapSize.width = 2048; // Shadow map size
-sunLight.shadow.mapSize.height = 2048;
-sunLight.shadow.camera.near = 0.5;
-sunLight.shadow.camera.far = 500;
-scene.add(sunLight);
+addSunLight();
 
 
 
@@ -34,8 +56,8 @@ scene.add(sunLight);
 function animate() {
     requestAnimationFrame(animate);
 
-    sun.children[0].rotation.y += 0.002;
-    // sun.children[1].rotation.y += 0.002;
+    // Update planet positions
+    updatePlanetPositions();
     stars.rotation.y -= 0.0002;
 
     // Render the scene
@@ -44,3 +66,78 @@ function animate() {
 
 // Start the animation loop
 animate();
+
+
+
+// helpers
+
+
+function addSunLight() {
+  const sunWorldRadius = sun.scale.x+ 0.1;
+  const sunLight1 = new THREE.PointLight(0xffffff);
+  const sunLight2 = new THREE.PointLight(0xffffff);
+  const sunLight3 = new THREE.PointLight(0xffffff);
+  const sunLight4 = new THREE.PointLight(0xffffff);
+  const sunLight5 = new THREE.PointLight(0xdddddd);
+  const sunLight6 = new THREE.PointLight(0xdddddd);
+  sunLight1.position.set(-sunWorldRadius, 0, 0);
+  sunLight2.position.set(sunWorldRadius, 0, 0);
+  sunLight3.position.set(0, 0, sunWorldRadius);
+  sunLight4.position.set(0, 0, -sunWorldRadius);
+  sunLight5.position.set(0, -sunWorldRadius, 0);
+  sunLight6.position.set(0, sunWorldRadius, 0);
+  enableShadows(sunLight1);
+  enableShadows(sunLight2);
+  enableShadows(sunLight3);
+  enableShadows(sunLight4);
+  enableShadows(sunLight5);
+  enableShadows(sunLight6);
+}
+
+function enableShadows(sunLight) {
+  sunLight.castShadow = true; // Enable shadow casting
+  sunLight.shadow.mapSize.width = 2048; // Shadow map size
+  sunLight.shadow.mapSize.height = 2048;
+  sunLight.shadow.camera.near = 0.5;
+  sunLight.shadow.camera.far = 500;
+  scene.add(sunLight);
+}
+
+
+// update planet positions
+function updatePlanetPositions() {
+  updateSunPosition();
+  // updateEarthAndMoonPosition(); // TODO: fix this
+}
+
+// Update sun positions
+function updateSunPosition() {
+  sun.children[0].rotation.y += 0.002;
+  sun.children[1].rotation.y += 0.002;
+}
+
+
+// Update earth and moon position
+function updateEarthAndMoonPosition() {
+  // Update earth and moon rotations
+  earth.children[0].rotation.y += 0.002; // 1/500 (1 day = 500 seconds in the model)
+  earth.children[1].rotation.y += 0.002;
+  earth.children[2].rotation.y += 0.0026;
+  earth.children[3].rotation.y += 0.002;
+  moon.children[0].rotation.y += 0.002; // moon rotates at same speed as earth
+
+  // Constants
+  const sunMass = 1.989 * Math.pow(10, 30); // kg
+  const earthMass = 5.972 * Math.pow(10, 24); // kg
+  const moonMass = 7.342 * Math.pow(10, 22); // kg
+  const moonOrbitRadius = 384400 * 1000; // meters
+  const earthOrbitRadius = 149.6 * Math.pow(10, 6) * 1000; // meters
+
+  // Update earth position
+  const { newEarthX, newEarthZ } = getOrbitalPosition(earthOrbitRadius, sunMass, earthMass, earth, sun);
+  earth.position.set(newEarthX, 0, newEarthZ);
+
+  // Update moon position
+  const { newMoonX, newMoonZ } = getOrbitalPosition(moonOrbitRadius, earthMass, moonMass, moon, earth);
+  moon.position.set(newMoonX, 0, newMoonZ);
+}
--- utilities/planet_builder/sunBuilder.js
+++ utilities/planet_builder/sunBuilder.js
@@ -20,23 +20,19 @@ function sunBuilder() {
     const material = new THREE.MeshPhongMaterial({
         map: loader.load("../textures/sunmap.jpg"),
         emissive: new THREE.Color(0xffffff), // Set the emissive color to white or any other bright color
-        emissiveIntensity: 0.05, // Increase the emissive intensity to make the rings brighter
+        emissiveIntensity: 1, // Increase the emissive intensity to make the rings brighter
     });
 
     // // create atmospheric glow
-    const fresnelMat = getFresnelMat(0xebb515, 0xebb515);
+    const fresnelMat = getFresnelMat(0xebb515, 0x000000);
 
 
 
 
     // create meshes for each material
     const sunMesh = new THREE.Mesh(geometry, material);
-    sunMesh.castShadow = false;
-    sunMesh.receiveShadow = false;
 
     const glowMesh = new THREE.Mesh(geometry, fresnelMat);
-    glowMesh.castShadow = false;
-    glowMesh.receiveShadow = false;
     glowMesh.scale.setScalar(1.01);
 
 
@@ -44,10 +40,6 @@ function sunBuilder() {
     // add elements to earthGroup in order
     sunGroup.add(sunMesh);           // sunGroup.children[0] 
     sunGroup.add(glowMesh);          // sunGroup.children[1]
-
-    // Hemisphere light to simulate ambient lighting
-    const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
-    sunGroup.add(hemiLight);
 
 
     // scale earth group
